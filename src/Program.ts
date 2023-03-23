478baf8c6ce6ad825144de7343e970abf6337db3
Brendan Bycroft <brendan.bycroft@gmail.com>
2023-03-23 21:50:00 +1300

    Attempt prod bug fix

--- src/Program.ts
+++ src/Program.ts
@@ -79,7 +79,7 @@ export function initProgramState(canvasEl: HTMLCanvasElement, fontAtlasData: IFo
         render,
         walkthrough,
         camera,
-        shape,
+        shape: shape,
         layout: genGptModelLayout(shape),
         gptGpuModel: null,
         markDirty: () => { },
