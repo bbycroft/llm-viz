d16a7c1d28b95137c1a8f95fdf9376f703e25550
Brendan Bycroft <brendan.bycroft@gmail.com>
2026-08-09 13:48:22 +1200

    remove vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import s from './layout.module.scss';
 import '@/styles/main.css';
-import { inject } from '@vercel/analytics';
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false;
@@ -12,10 +11,6 @@ export default function RootLayout({
 }: {
     children: React.ReactNode;
 }) {
-    useEffect(() => {
-         inject();
-    }, []);
-
     return <html lang="en" className={s.html}>
         <head>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto&family=Merriweather:ital@0;1&display=optional" />
